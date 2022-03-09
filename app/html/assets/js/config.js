89ba06ec4be0bcb20528dbf47d98a14291bba2c3
Ben Lanning <blanning@all-mode.com>
2022-03-09 16:13:38 -0500

    Add .gitignore, pull .env and config.js

--- app/html/assets/js/config.js
+++
@@ -1,8 +0,0 @@
-var env = {
-  // host: 'net.amc.local',
-  host: 'localhost',
-  port: '8080',
-  debug: false,
-  vmhost: 'vcenter.amc.local',
-  cred: 'Basic dnNwaGVyZS5sb2NhbFxyZXN0OkxldE1lQzBubjNjdCEq',
-};
